303edd213fa3d9c4a35fabded6d35ccac6049f24
sprout2000 <k_konno1983@yahoo.co.jp>
2019-11-18 15:12:12 +0900

    Update demo page

--- demo/app.tsx
+++ demo/app.tsx
@@ -1,48 +1,15 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import EvnetListener from 'react-event-listener';
-import styled, { createGlobalStyle } from 'styled-components';
 
 import PanZoom from '../src/index';
-
-const GlobalStyle = createGlobalStyle`
-  html, body, #root {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-  }
-`;
-
-const Container = styled.div`
-  height: 100%;
-`;
-
-const Header = styled.div`
-  padding: 2em;
-  text-align: center;
-  color: #666;
-`;
-
-const Content = styled.div`
-  height: 80vmin;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ImgContainer = styled.div`
-  margin: 0;
-  padding: 0;
-  height: 80%;
-  width: 80%;
-`;
 
 const App = (): JSX.Element => {
   const [url, setUrl] = useState('https://source.unsplash.com/random');
 
   const regexp = /bmp|ico|gif|jpeg|png|apng|svg|webp/;
   const agent = window.navigator.userAgent.toLowerCase();
-  const mac = agent.includes('mac os x');
+  const macos = agent.includes('mac os x');
 
   const preventDefault = (e: DragEvent): void => {
     e.preventDefault();
@@ -73,9 +40,7 @@ const App = (): JSX.Element => {
   };
 
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <Container>
+      <div className="container">
         <EvnetListener
           target="window"
           onDragEnter={(e): void => preventDefault(e)}
@@ -83,20 +48,19 @@ const App = (): JSX.Element => {
           onDragLeave={(e): void => preventDefault(e)}
           onDrop={(e): void => handleOnDrop(e)}
         />
-        <Header>You can drop an image file here... </Header>
-        <Content>
-          <ImgContainer>
+        <div className="header">You can drop an image file here... </div>
+        <div className="content">
+          <div className="image-container">
             <PanZoom
               url={url}
-              zoomSnap={mac ? 0.3 : 0}
+              zoomSnap={macos ? 0.3 : 0}
               doubleClickReset
               getFocus
               attribution='<a target="_blank" href="https://source.unsplash.com/">source.unsplash.com</a>'
             />
-          </ImgContainer>
-        </Content>
-      </Container>
-    </React.Fragment>
+          </div>
+        </div>
+      </div>
   );
 };
 
