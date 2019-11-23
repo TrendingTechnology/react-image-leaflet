7923adb5179a3c2e2689db50e217d7bd4e89736a
sprout2000 <k_konno1983@yahoo.co.jp>
2019-11-24 08:36:26 +0900

    Update Demo

--- demo/app.tsx
+++ demo/app.tsx
@@ -1,8 +1,40 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import styled, { createGlobalStyle } from 'styled-components';
 import EvnetListener from 'react-event-listener';
 
 import PanZoom from '../src/index';
+
+const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
+`;
+
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.p`
+  font-size: 2.5vh;
+  color: #666;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`;
+
+const Leaflet = styled.div`
+  width: 80%;
+  height: 70%;
+`;
 
 const App = (): JSX.Element => {
   const [url, setUrl] = useState('https://source.unsplash.com/random');
@@ -39,17 +71,18 @@ const App = (): JSX.Element => {
   };
 
   return (
-    <div className="container">
-      <EvnetListener
-        target="window"
-        onDragEnter={(e): void => preventDefault(e)}
-        onDragOver={(e): void => preventDefault(e)}
-        onDragLeave={(e): void => preventDefault(e)}
-        onDrop={(e): void => handleOnDrop(e)}
-      />
-      <div className="header">You can also drop an image file here... </div>
-      <div className="content">
-        <div className="image-container">
+    <React.Fragment>
+      <GlobalStyle />
+      <Container>
+        <EvnetListener
+          target="window"
+          onDragEnter={(e): void => preventDefault(e)}
+          onDragOver={(e): void => preventDefault(e)}
+          onDragLeave={(e): void => preventDefault(e)}
+          onDrop={(e): void => handleOnDrop(e)}
+        />
+        <Text>You can also drop an image file here... </Text>
+        <Leaflet>
           <PanZoom
             url={url}
             zoomSnap={macos ? 0.3 : 0}
@@ -57,9 +90,9 @@ const App = (): JSX.Element => {
             getFocus
             attribution='<a target="_blank" href="https://source.unsplash.com/">source.unsplash.com</a>'
           />
-        </div>
-      </div>
-    </div>
+        </Leaflet>
+      </Container>
+    </React.Fragment>
   );
 };
 
